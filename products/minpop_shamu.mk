eafd12fe4cabcf57a112ff6d532f08c40cc43154
Michael J. Cusack <WhoNeedszZz@gmail.com>
2015-05-16 18:13:55 -0500

    Revert: Change to full instead of aosp

--- products/minpop_shamu.mk
+++ products/minpop_shamu.mk
@@ -1,5 +1,5 @@
 # Inherit AOSP device configuration for shamu.
-$(call inherit-product, device/moto/shamu/full_shamu.mk)
+$(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
 # Inherit common product files.
 include vendor/minpop/products/common.mk
