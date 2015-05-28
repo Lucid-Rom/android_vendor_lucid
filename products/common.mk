31ce7bf067f47d8577d07d8d70d72973d227409e
Michael J. Cusack <WhoNeedszZz@gmail.com>
2015-05-28 13:07:58 -0500

    Enable adb authentication
    Change-Id: I21fa13b33dc5e9ef446f8e13686b19d92df77eea
    Signed-off-by: Michael J. Cusack <WhoNeedszZz@gmail.com>

--- products/common.mk
+++ products/common.mk
@@ -22,6 +22,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true \
     persist.sys.dun.override=0 \
     ro.build.selinux=1 \
+    ro.adb.secure=1 \
     ro.config.vc_music_vol_steps=30 \
     ro.setupwizard.network_required=false \
     ro.setupwizard.gservices_delay=-1 \
